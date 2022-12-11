c1220c07ebeb02f5a83fd7f38723b6d6d285b87c
Dubass83 <makssych@gmail.com>
2022-12-11 20:19:39 +0200

    add test for topoSort function

--- pkg/sort/sort_test.go
+++ pkg/sort/sort_test.go
@@ -1,6 +1,9 @@
 package sort
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestQuikSort(t *testing.T) {
 	testSlice := []int{9, 1, 2, 4, 5, 3, 6, 8, 7}
@@ -19,7 +22,61 @@ func TestQuikSort(t *testing.T) {
 	}
 }
 
-func compareSlice(foo, bar []int) bool {
+func TestTopoSort(t *testing.T) {
+	var prereqs = map[string][]string{
+		"algorithms": {"data structures"},
+		"calculus":   {"linear algebra"},
+		"compilers": {
+			"data structures",
+			"formal languages",
+			"computer organization",
+		},
+		"data structures":       {"discrete math"},
+		"databases":             {"data structures"},
+		"discrete math":         {"intro to programming"},
+		"formal languages":      {"discrete math"},
+		"networks":              {"operating systems"},
+		"operating systems":     {"data structures", "computer organization"},
+		"programming languages": {"data structures", "computer organization"},
+	}
+
+	var expectedResult = []string{
+		"intro to programming",
+		"discrete math",
+		"data structures",
+		"algorithms",
+		"linear algebra",
+		"calculus",
+		"formal languages",
+		"computer organization",
+		"compilers",
+		"databases",
+		"operating systems",
+		"networks",
+		"programming languages",
+	}
+
+	actualResult := topoSort(prereqs)
+	if !compareSlice(actualResult, expectedResult) {
+		for i, val := range actualResult {
+			fmt.Printf("%d:\t%s\n", i+1, val)
+		}
+		t.Errorf("Slice is not sorted well!\n"+
+			"actualResult:\t%v\nexpectedResult:\t%v", actualResult, expectedResult)
+	}
+
+	actualResult = TopoSort(prereqs)
+	if !compareSlice(actualResult, expectedResult) {
+		for i, val := range actualResult {
+			fmt.Printf("%d:\t%s\n", i+1, val)
+		}
+		t.Errorf("Slice is not sorted well!\n"+
+			"actualResult:\t%v\nexpectedResult:\t%v", actualResult, expectedResult)
+	}
+
+}
+
+func compareSlice[V int | string](foo, bar []V) bool {
 	if len(foo) != len(bar) {
 		return false
 	}
