7d9dcac4d6eb8416eaeff20d2ba756101e87fba2
dubass83 <makssych@gmail.com>
2023-02-13 19:31:01 +0200

    feat: add BFS function

--- pkg/search/search.go
+++ pkg/search/search.go
@@ -30,3 +30,32 @@ func BinaryInt(sl []int, val int) (find int, err error) {
 	err = fmt.Errorf("no such value in the list: %d", val)
 	return -1, err
 }
+
+func BFS(start string, graph map[string][]string, check func(name string) bool) bool {
+	var search_queue []string
+	search_queue = append(search_queue, graph[start]...)
+	var searched []string
+	var person string
+	for len(search_queue) != 0 {
+		person = search_queue[0]
+		search_queue = search_queue[1:]
+		if personNotInSearched(person, searched) {
+			if check(person) {
+				fmt.Println("find: ", person)
+				return true
+			}
+			search_queue = append(search_queue, graph[person]...)
+			searched = append(searched, person)
+		}
+	}
+	return false
+}
+
+func personNotInSearched(name string, serched []string) bool {
+	for _, n := range serched {
+		if n == name {
+			return false
+		}
+	}
+	return true
+}
