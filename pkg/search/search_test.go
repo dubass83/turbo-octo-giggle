49bd5134fc4b3ccdf9e6b1db67c2b090173de56e
dubass83 <makssych@gmail.com>
2023-02-13 20:15:20 +0200

    feat: add test for BFS

--- pkg/search/search_test.go
+++ pkg/search/search_test.go
@@ -13,3 +13,27 @@ func TestBinaryInt(t *testing.T) {
 		t.Errorf("expectedResult: %d not equil actualResult: %d", expectedResult, actualResult)
 	}
 }
+
+func TestBFS(t *testing.T) {
+	var graph = make(map[string][]string)
+	graph["you"] = []string{"alice", "bob", "claire"}
+	graph["bob"] = []string{"anuj", "peggy"}
+	graph["alice"] = []string{"peggy"}
+	graph["claire"] = []string{"thom", "jonny"}
+	graph["anuj"] = []string{}
+	graph["peggy"] = []string{}
+	graph["thom"] = []string{}
+	graph["jonny"] = []string{}
+	checkPersonM := func(name string) bool {
+		return name[len(name)-1] == 'm'
+	}
+	checkPersonX := func(name string) bool {
+		return name[len(name)-1] == 'x'
+	}
+	if !BFS("you", graph, checkPersonM) {
+		t.Errorf("unexpected")
+	}
+	if BFS("you", graph, checkPersonX) {
+		t.Errorf("expected")
+	}
+}
